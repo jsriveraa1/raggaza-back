59020c3ab1e63b8bcffce1cb8b90ef12916ef193
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2023-01-14 16:07:48 -0500

    Se agrega el bean @CrossOrigin

--- src/main/java/com/co/ragazza/backend/ragazza/infrastructure/rest/spring/resources/AccesorioController.java
+++ src/main/java/com/co/ragazza/backend/ragazza/infrastructure/rest/spring/resources/AccesorioController.java
@@ -7,6 +7,7 @@ import com.co.ragazza.backend.ragazza.infrastructure.rest.spring.mapper.Accesori
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("accesorio")
+@CrossOrigin(value = "*", origins = "*")
 public class AccesorioController {
 
     private final AccesorioLogic accesorioLogic;
